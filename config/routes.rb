7d785b750d728ba3e7b1dcfe402934b97b9e734f
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-06 01:06:09 +0200

    homepage ok, startdating en cours

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'participants/new'
   devise_for :users
   root to: 'pages#home'
+  get "/startdating", to: "pages#startdating", as: "startdating"
   resources :datings, only: [ :index, :new, :create] do
     resources :participants, only: [ :new, :create ]
   end
