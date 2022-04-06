b0fc99540aa21a6c5d8191ad5ae7039fe86dfa1c
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-06 13:00:16 +0200

    form new participant, navbar ok, liens home revus

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
   get 'participants/new'
   devise_for :users
   root to: 'pages#home'
   get "/startdating", to: "pages#startdating", as: "startdating"
-  resources :datings, only: [ :index, :new, :create ] do
+  match '/users', to: 'users#index', via: 'get'
+  resources :datings, only: [ :index, :new, :create, :show ] do
     resources :participants, only: [ :new, :create ]
   end
 end
