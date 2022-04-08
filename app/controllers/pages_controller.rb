876d99e8830a8c603fa14b46d8e1f5ab7341ef30
marie sepanik <sepanik.marie@gmail.com>
2022-04-08 09:14:01 +0200

    ajout création nouveau date depuis la home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :startdating ]
 
   def home
+    @dating = Dating.new
   end
 
   def startdating
