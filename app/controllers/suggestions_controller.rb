b795b1c0a84141c1742d19120a7b0addad267d84
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-09 19:23:04 +0200

    controller suggestions index avec les @markers pour la map

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,5 +2,9 @@ class SuggestionsController < ApplicationController
   def index
     @dating = Dating.find(params[:dating_id])
     @suggestions = Suggestion.where(dating: @dating)
+    @bars = []
+    @suggestions.each do |suggestion|
+      @bars << suggestion.bar
+    end
   end
 end
