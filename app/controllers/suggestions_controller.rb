56e3460fab268435246aa72446459ba1953f34f8
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-09 19:25:23 +0200

    bug dans l'index avec les markers

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -6,5 +6,13 @@ class SuggestionsController < ApplicationController
     @suggestions.each do |suggestion|
       @bars << suggestion.bar
     end
+    # the `geocoded` scope filters only bars with coordinates (latitude & longitude)
+    @markers = @bars.geocoded.map do |bar|
+      {
+        lat: bar.latitude,
+        lng: bar.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { bar: bar })
+      }
+    end
   end
 end
