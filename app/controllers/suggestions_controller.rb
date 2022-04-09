fa4605e47cf039ec52db9fe00f900b64b5c25e37
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-10 00:38:14 +0200

    geocoding buggé

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -2,17 +2,8 @@ class SuggestionsController < ApplicationController
   def index
     @dating = Dating.find(params[:dating_id])
     @suggestions = Suggestion.where(dating: @dating)
-    @bars = []
-    @suggestions.each do |suggestion|
-      @bars << suggestion.bar
-    end
+
     # the `geocoded` scope filters only bars with coordinates (latitude & longitude)
-    @markers = @bars.geocoded.map do |bar|
-      {
-        lat: bar.latitude,
-        lng: bar.longitude,
-        info_window: render_to_string(partial: "info_window", locals: { bar: bar })
-      }
-    end
+
   end
 end
