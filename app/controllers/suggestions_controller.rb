29dcdaa76f8cd4f51a1701d7edda27c3881919cb
Myriam Delbreil <myriam.delbreil@live.fr>
2022-04-10 01:03:41 +0200

    map fonctionnelle

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -4,6 +4,11 @@ class SuggestionsController < ApplicationController
     @suggestions = Suggestion.where(dating: @dating)
 
     # the `geocoded` scope filters only bars with coordinates (latitude & longitude)
-
+    @markers = @dating.bars.geocoded.map do |bar| {
+      lat: bar.latitude,
+      lng: bar.longitude,
+      info_window: render_to_string(partial: "info_window", locals: { bar: bar })
+      }
+    end
   end
 end
--- app/models/dating.rb
+++ app/models/dating.rb
@@ -1,6 +1,8 @@
 class Dating < ApplicationRecord
   has_many :participants
   has_many :suggestions
+  has_many :bars, through: :suggestions
+
   PREFERENCES = ['Bière', 'Vin', 'Cocktail', 'Soft']
   PRICE_RANGES = ['€', '€€', '€€€', '€€€€']
   ARRONDISSEMENTS = ['75001', '75002', '75003', '75004', '75005', '75006', '75007', '75008', '75009','75010', '75011', '75012', '75013', '75014', '75015', '75016', '75017', '75018', '75019', '75020']
